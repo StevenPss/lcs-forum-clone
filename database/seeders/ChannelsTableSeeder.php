52f84ab230c8503aa6ef8e16e4d158873e899544
StevenPss <pulessteven@gmail.com>
2021-01-17 10:50:29 +0200

    refactor: added more channels

--- database/seeders/ChannelsTableSeeder.php
+++ database/seeders/ChannelsTableSeeder.php
@@ -16,23 +16,103 @@ class ChannelsTableSeeder extends Seeder
     public function run()
     {
         Channel::create([
-            'name' => 'Laravel 8',
-            'slug' => Str::slug('Laravel 8')
+            'name' => 'Assistance',
+            'slug' => Str::slug('Code Review')
         ]);
 
         Channel::create([
-            'name' => 'React Js',
-            'slug' => Str::slug('React Js')
+            'name' => 'Eloquent',
+            'slug' => Str::slug('Eloquent')
         ]);
 
         Channel::create([
-            'name' => 'Boostrap 5',
-            'slug' => Str::slug('Boostrap 5')
+            'name' => 'Envoyer',
+            'slug' => Str::slug('Envoyer')
         ]);
 
         Channel::create([
-            'name' => 'NodeJS',
-            'slug' => Str::slug('Node Js')
+            'name' => 'Feedback',
+            'slug' => Str::slug('Site Improvements')
+        ]);
+
+        Channel::create([
+            'name' => 'Forge',
+            'slug' => Str::slug('Forge')
+        ]);
+
+        Channel::create([
+            'name' => 'General',
+            'slug' => Str::slug('General Discussion')
+        ]);
+
+        Channel::create([
+            'name' => 'Guides',
+            'slug' => Str::slug('Guides')
+        ]);
+
+        Channel::create([
+            'name' => 'JavaScript',
+            'slug' => Str::slug('JavaScript')
+        ]);
+
+        Channel::create([
+            'name' => 'Laravel',
+            'slug' => Str::slug('Laravel')
+        ]);
+
+        Channel::create([
+            'name' => 'Livewire',
+            'slug' => Str::slug('Livewire')
+        ]);
+
+        Channel::create([
+            'name' => 'Lumen',
+            'slug' => Str::slug('Lumen')
+        ]);
+
+        Channel::create([
+            'name' => 'Mix',
+            'slug' => Str::slug('Elixir')
+        ]);
+
+        Channel::create([
+            'name' => 'Nova',
+            'slug' => Str::slug('Nova')
+        ]);
+
+        Channel::create([
+            'name' => 'Requests',
+            'slug' => Str::slug('Requests')
+        ]);
+
+        Channel::create([
+            'name' => 'Servers',
+            'slug' => Str::slug('Servers')
+        ]);
+
+        Channel::create([
+            'name' => 'Spark',
+            'slug' => Str::slug('Spark')
+        ]);
+
+        Channel::create([
+            'name' => 'Testing',
+            'slug' => Str::slug('Testing')
+        ]);
+
+        Channel::create([
+            'name' => 'Tips',
+            'slug' => Str::slug('Tips')
+        ]);
+
+        Channel::create([
+            'name' => 'Vapor',
+            'slug' => Str::slug('Vapor')
+        ]);
+
+        Channel::create([
+            'name' => 'Vue',
+            'slug' => Str::slug('Vue')
         ]);
     }
 }
