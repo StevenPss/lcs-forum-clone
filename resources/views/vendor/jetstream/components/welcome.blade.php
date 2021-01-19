36b3119b7efbb150925bb9022edc0403fca9c4b0
StevenPss <pulessteven@gmail.com>
2021-01-19 11:43:03 +0200

    style: added sidebar

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -1,7 +1,51 @@
 <div class="grid grid-cols-1 md:grid-cols-4">
 
     <div class="flex flex-col flex-col-reverse md:flex-row mx-auto" style="max-width: 1070px;">
-        a
+        <!--sidebar-->
+        <div class="hidden lg:block lg:sticky mr-9 lg:items-start flex-none" style="min-width:200px; top:40px;">
+            <div class="lg:sticky text-center">
+                <a id="js-new-discussion-button" href="#" class="bg-blue-400 inline-flex justify-center w-full rounded-xl px-4 py-2 text-sm font-bold text-white hover:bg-blue-500 mb-8 uppercase" style="box-shadow: rgb(215, 232, 253) 0px 4px 10px 1px;">
+                    New Discussion
+                </a>
+                <ul>
+                    <li>
+                        <a id="js-new-discussion-button" href="#" class="bg-blue-50 inline-flex justify-start items-center w-full rounded-xl px-4 py-2 text-xs font-semibold text-blue-400 border border-blue-400 hover:bg-blue-500 mb-1 capitalize" style="height:40px;">
+                            <i class="fas fa-database text-sm mr-4"></i> all threads
+                        </a>
+                    </li>
+                    <li>
+                        <a id="js-new-discussion-button" href="#" class="inline-flex justify-start items-center w-full rounded-xl px-4 py-2 text-xs font-semibold hover:text-blue-400 text-gray-400 border hover:border-blue-400 hover:bg-blue-50 mb-1 capitalize" style="height:40px;">
+                            <i class="fas fa-star text-sm mr-4"></i> popular this week
+                        </a>
+                    </li>
+                    <li>
+                        <a id="js-new-discussion-button" href="#" class="inline-flex justify-start items-center w-full rounded-xl px-4 py-2 text-xs font-semibold hover:text-blue-400 text-gray-400 border hover:border-blue-400 hover:bg-blue-50 mb-1 capitalize" style="height:40px;">
+                            <i class="fas fa-star text-sm mr-4"></i> popular all time
+                        </a>
+                    </li>
+                    <li>
+                        <a id="js-new-discussion-button" href="#" class="inline-flex justify-start items-center w-full rounded-xl px-4 py-2 text-xs font-semibold hover:text-blue-400 text-gray-400 border hover:border-blue-400 hover:bg-blue-50 mb-1 capitalize" style="height:40px;">
+                            <i class="fas fa-check-circle text-sm mr-4"></i> solved
+                        </a>
+                    </li>
+                    <li>
+                        <a id="js-new-discussion-button" href="#" class="inline-flex justify-start items-center w-full rounded-xl px-4 py-2 text-xs font-semibold hover:text-blue-400 text-gray-400 border hover:border-blue-400 hover:bg-blue-50 mb-1 capitalize" style="height:40px;">
+                            <i class="fas fa-times-circle text-sm mr-4"></i> unsolved
+                        </a>
+                    </li>
+                    <li>
+                        <a id="js-new-discussion-button" href="#" class="inline-flex justify-start items-center w-full rounded-xl px-4 py-2 text-xs font-semibold hover:text-blue-400 text-gray-400 border hover:border-blue-400 hover:bg-blue-50 mb-1 capitalize" style="height:40px;">
+                            <i class="fas fa-reply text-sm mr-4"></i> no replies yet
+                        </a>
+                    </li>
+                    <li>
+                        <a id="js-new-discussion-button" href="#" class="inline-flex justify-start items-center w-full rounded-xl px-4 py-2 text-xs font-semibold hover:text-blue-400 text-gray-400 border hover:border-blue-400 hover:bg-blue-50 mb-1 capitalize" style="height:40px;">
+                            <i class="fas fa-clipboard text-sm mr-4"></i> leaderboard
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div><!--end of sidebar-->
     </div>
 
     <!--forum main-->
